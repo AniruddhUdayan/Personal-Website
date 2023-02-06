7d3d8f959a52b14db7bb9760324c84790d831ca8
Aniruddh Udayan <aniruddhudayan34@gmail.com>
2023-02-07 00:09:33 +0530

    Changed resume and added portfolio in projects

--- src/UI/buttons/DownloadBtn.js
+++ src/UI/buttons/DownloadBtn.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './DownloadBtn.css';
 import { FaDownload } from 'react-icons/fa';
-import CV from '../../assets/resume.pdf';
+import CV from '../../assets/Myresume.pdf';
 
 function DownloadBtn() {
   return (
-    <a href={CV} download className="button" data-tooltip="Cv file: 20Mb">
+    <a href={CV} download className="button" data-tooltip="Cv file: 45kb">
       <div className="button-wrapper"></div>
       <div className="text">Download CV</div>
       <span className="icon">
